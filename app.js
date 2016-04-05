1bf17b2b21723d1703890f57aed2239973de7786
jeffslutzky <jeffslutzky@gmail.com>
2016-04-05 13:28:13 -0400

    add validations and create object

--- app.js
+++ app.js
@@ -14,16 +14,41 @@ $(function() {
 
   $(".survey").on("click", ".delete-question", function(){
     $(this).parent().parent().remove();
-  })
+  });
 
   $(".submit").on("click", function(){
+    $(".message").empty();
+    var object = [];
     var title = $("input[name='title']").val();
     var points = $("input[name='points']").val();
     var description = $("input[name='description']").val();
+    if (title.length < 100) {
+      object.push({title: title});
+    } else {
+      $(".message").append("<p>Title must be less than 100 characters.</p>");
+    };
+    if ($.isNumeric(points) && points > 0) {
+      object.push({pointValue: points});
+    } else {
+      $(".message").append("<p>Point value must be a positive integer.</p>");
+    };
+    if (description.length < 500) {
+      object.push({description: description});
+    } else {
+      $(".message").append("<p>Description must be less than 500 characters.</p>");
+    };
+    object.push({title: title, pointValue: points, description: description});
     var questions = [];
     $("input[name='question']").each(function(){
-      questions.push($(this).val());
-    })
-  })
+      questions.push({title: $(this).val()});
+    });
+    object.push(questions);
+  });
+
+
 
 })
+
+var validate = function(){
+  debugger;
+};
