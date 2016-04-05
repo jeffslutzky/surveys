e6fca58214fe871bde8449418c4531119c7c05c6
jeffslutzky <jeffslutzky@gmail.com>
2016-04-04 22:31:07 -0400

    complete form

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ $(function() {
     $(".survey").addClass("survey-visible").removeClass("survey-hidden");
   });
 
-  $(".question-button").on("click", function(){
-    debugger;
+  $(".add-question").on("click", function(){
+    $(this).before("<div class='question'>\
+    <p>Question <input type='text' name='question'> \
+    <span class='delete-question'><button type='button'>delete</button></span>\
+    </p>\
+    </div>");
   });
+
+  $(".survey").on("click", ".delete-question", function(){
+    $(this).parent().remove();
+  })
 
 })
